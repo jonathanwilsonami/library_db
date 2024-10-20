058a05e64e3fca323344357828d4118d445c7959
jonathanwilsonami <jonwilami@gmail.com>
2024-10-20 14:49:16 -0400

    Update diagrams and schema

--- library_create_schema.sql
+++ library_create_schema.sql
@@ -14,22 +14,12 @@ $do$;
 -- Checks
 -- SELECT datname FROM pg_database;
 
-
 -- Create Relation Schemas 
 
--- Represents individual items available in the library, such as books, magazines, e-books, and audiobooks.
-CREATE TABLE IF NOT EXISTS Material (
-    material_id SERIAL PRIMARY KEY, -- A unique identﬁer for each material.
-    title VARCHAR(500), -- The title of the material.
-    publication_date DATE, -- The date of publication of the material
-	catalog_id INT, -- A reference to the catalog entry for the material.
-	genre_id INT -- A reference to the genre of the material.
-);
-
--- Represents a record of library materials with informa<on on their availability and location.
+-- Represents a record of library materials with information on their availability and location.
 CREATE TABLE IF NOT EXISTS Catalog (
     catalog_id SERIAL PRIMARY KEY, 
-    name VARCHAR(500), -- The name of the catalog.
+    name VARCHAR(500) NOT NULL, -- The name of the catalog.
 	location VARCHAR(500) -- The location of the material within the library.
 );
 
@@ -40,30 +30,21 @@ CREATE TABLE IF NOT EXISTS Genre (
     description TEXT -- A brief introduction or description of the genre.
 );
 
--- Represents the borrowing activity of library materials by members.
-CREATE TABLE IF NOT EXISTS Borrow (
-    borrow_id SERIAL PRIMARY KEY,
-    material_id INT NOT NULL,
-    member_id INT NOT NULL,
-    staff_id INT NOT NULL,
-    borrow_date DATE NOT NULL,
-    due_date DATE NOT NULL,
-    return_date DATE
-);
-
--- Represents authors who have created library materials.
-CREATE TABLE IF NOT EXISTS Author (
-    author_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL, -- The name of the author.
-    birth_date DATE,
-    nationality VARCHAR(100) -- The nationality of the author (optional).
-);
-
--- Represents the relationship between authors and the materials they have created.
-CREATE TABLE IF NOT EXISTS Authorship (
-    authorship_id SERIAL PRIMARY KEY,
-    author_id INT NOT NULL,
-    material_id INT NOT NULL
+-- Represents individual items available in the library, such as books, magazines, e-books, and audiobooks.
+CREATE TABLE IF NOT EXISTS Material (
+    material_id SERIAL PRIMARY KEY, -- A unique identﬁer for each material.
+    title VARCHAR(500) NOT NULL, -- The title of the material.
+    publication_date DATE, -- The date of publication of the material
+	catalog_id INT DEFAULT NULL, -- A reference to the catalog entry for the material.
+	genre_id INT DEFAULT NULL, -- A reference to the genre of the material.
+	FOREIGN KEY (catalog_id) 
+		REFERENCES Catalog(catalog_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE, 
+	FOREIGN KEY (genre_id) 
+		REFERENCES Genre(genre_id) 
+		ON DELETE SET NULL
+		ON UPDATE CASCADE
 );
 
 -- Represents library members who can borrow and reserve materials.
@@ -83,15 +64,57 @@ CREATE TABLE IF NOT EXISTS Staff (
     hire_date DATE NOT NULL -- The date the staff member was hired by the library.
 );
 
+-- Represents the borrowing activity of library materials by members.
+CREATE TABLE IF NOT EXISTS Borrow (
+    borrow_id SERIAL PRIMARY KEY,
+    material_id INT NOT NULL,
+    member_id INT NOT NULL,
+    staff_id INT NOT NULL,
+	borrow_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    due_date DATE NOT NULL,
+    return_date DATE,
+	FOREIGN KEY (material_id) 
+		REFERENCES Material(material_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE, 
+	FOREIGN KEY (member_id) 
+		REFERENCES Member(member_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE, 
+	FOREIGN KEY (staff_id) 
+		REFERENCES Staff(staff_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE, 
+	CONSTRAINT check_return_date CHECK (return_date IS NULL OR return_date >= borrow_date),
+    CONSTRAINT check_due_date CHECK (due_date >= borrow_date) 
+);
+
+-- Represents authors who have created library materials.
+CREATE TABLE IF NOT EXISTS Author (
+    author_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL, -- The name of the author.
+    birth_date DATE DEFAULT NULL,
+    nationality VARCHAR(100) DEFAULT NULL -- The nationality of the author (optional).
+);
+
+-- Represents the relationship between authors and the materials they have created.
+CREATE TABLE IF NOT EXISTS Authorship (
+    authorship_id SERIAL PRIMARY KEY,
+    author_id INT NOT NULL,
+    material_id INT NOT NULL,
+	FOREIGN KEY (author_id) 
+		REFERENCES Author(author_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE, 
+	FOREIGN KEY (material_id) 
+		REFERENCES Material(material_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
 -- init tables with sample data
 DO $$
 BEGIN
-    IF (SELECT COUNT(*) FROM Material) = 0 THEN
-        COPY Material(material_id, title, publication_date, catalog_id, genre_id)
-        FROM '/var/lib/postgresql/init_data/Material.csv'
-        DELIMITER ','
-        CSV HEADER;
-    END IF;
 	
 	IF (SELECT COUNT(*) FROM Catalog) = 0 THEN
         COPY Catalog(catalog_id, name, location)
@@ -103,6 +126,27 @@ BEGIN
 	IF (SELECT COUNT(*) FROM Genre) = 0 THEN
         COPY Genre(genre_id, name, description)
         FROM '/var/lib/postgresql/init_data/Genre.csv'
+        DELIMITER ','
+        CSV HEADER;
+    END IF;
+
+	IF (SELECT COUNT(*) FROM Material) = 0 THEN
+        COPY Material(material_id, title, publication_date, catalog_id, genre_id)
+        FROM '/var/lib/postgresql/init_data/Material.csv'
+        DELIMITER ','
+        CSV HEADER;
+    END IF;
+
+	IF (SELECT COUNT(*) FROM Member) = 0 THEN
+        COPY Member(member_id, name, contact_info, join_date)
+        FROM '/var/lib/postgresql/init_data/Member.csv'
+        DELIMITER ','
+        CSV HEADER;
+    END IF;
+
+    IF (SELECT COUNT(*) FROM Staff) = 0 THEN
+        COPY Staff(staff_id, name, contact_info, job_title, hire_date)
+        FROM '/var/lib/postgresql/init_data/Staff.csv'
         DELIMITER ','
         CSV HEADER;
     END IF;
@@ -127,20 +171,7 @@ BEGIN
         DELIMITER ','
         CSV HEADER;
     END IF;
-
-    IF (SELECT COUNT(*) FROM Member) = 0 THEN
-        COPY Member(member_id, name, contact_info, join_date)
-        FROM '/var/lib/postgresql/init_data/Member.csv'
-        DELIMITER ','
-        CSV HEADER;
-    END IF;
-
-    IF (SELECT COUNT(*) FROM Staff) = 0 THEN
-        COPY Staff(staff_id, name, contact_info, job_title, hire_date)
-        FROM '/var/lib/postgresql/init_data/Staff.csv'
-        DELIMITER ','
-        CSV HEADER;
-    END IF;
+	
 END $$;
 
 /*
--- library_queries.sql
+++ library_queries.sql
@@ -1,38 +1,74 @@
 ---- Utils -------
 
--- select * from Catalog; 
+select * from Borrow; 
 
 -- call drop_all_objects_in_schema('public')
 
 ---- QUESTIONS -------
 
--- Which materials are currently available in the library?
+-- 1. Which materials are currently available in the library? A material is considered unavailable 
+-- if it has been borrowed and not yet returned.
+-- select * from Material;
 
--- A material is considered unavailable if it has been borrowed and not yet returned.
--- Which materials are currently overdue?
+-- 2. Which materials are currently overdue? Suppose today is 04/01/2023, and show the
+-- borrow date and due date of each material.
 
--- Assume today’s date is 04/01/2023. Display the borrow date and due date for each overdue material.
--- What are the top 10 most borrowed materials in the library?
+-- Get materials that were overdue in the past
+-- select material_id, title 
+-- from Material 
+-- where (material_id) IN (select material_id 
+-- 				from Borrow
+-- 				where return_date > due_date);
 
--- Display the title of each material, ordered by the number of times they’ve been borrowed.
--- How many materials has the author Lucas Piki written?
+-- Get materials that are currently overdue
+-- select material_id, title 
+-- from Material 
+-- where (material_id) IN (select material_id 
+-- 				from Borrow
+-- 				where CURRENT_DATE > due_date AND return_date IS NULL);
 
--- How many materials were written by two or more authors?
+-- Set arbitrary date variable
+WITH today_date AS (
+    SELECT '04/01/2023'::DATE AS date_value
+)
+SELECT borrow_date, due_date
+FROM Borrow
+WHERE  = (SELECT date_value FROM today_date);
 
--- What are the most popular genres in the library?
+-- select material_id, title 
+-- from Material 
+-- where (material_id) IN (select material_id 
+-- 				from Borrow
+-- 				where return_date > due_date);
 
--- Rank the genres by the total number of times materials in each genre have been borrowed.
--- How many materials were borrowed between 09/2020 and 10/2020?
+-- 3. What are the top 10 most borrowed materials in the library? Show the title of each
+-- material and order them based on their available counts.
 
--- How do you update the status of "Harry Potter and the Philosopher's Stone" when it is returned on 04/01/2023?
+-- 4. How many materials has the author Lucas Piki written?
 
--- How do you delete the member Emily Miller and all her related records from the database?
+-- 5. How many materials were written by two or more authors?
+-- select count(*) 
+-- from (select material_id 
+-- 		from Authorship 
+-- 		group by material_id
+-- 		having count(author_id) > 1);
 
--- How do you add the following material to the database?
+-- 6. What are the most popular genres in the library ranked by the total number of borrowed
+-- times of each genre?
 
-	/* Title: New book
-	Date: 2020-08-01
-	Catalog: E-Books
-	Genre: Mystery & Thriller
-	Author: Lucas Luke
-	*/
+-- 7. How many materials had been borrowed from 09/2020-10/2020?
+
+-- 8. How do you update the “Harry Poper and the Philosopher's Stone” when it is returned on
+-- 04/01/2023?
+
+-- 9. How do you delete the member Emily Miller and all her related records from the database?
+
+-- 10. How do you add the following material to the database?
+
+/*
+Title: New book
+Date: 2020-08-01
+Catalog: E-Books
+Genre: Mystery & Thriller
+Author: Lucas Luke
+*/
